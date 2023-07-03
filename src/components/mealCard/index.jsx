78fc3e9d89e0b6a2a6303e8bccf66176b05bc160
htahrawi <h.tahrawi01@gmail.com>
2023-07-03 14:02:06 +0300

    the menu section coded

---
+++ src/components/mealCard/index.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+class MealCard extends Component {
+    render() {
+        return (
+            <div className="menu__card">
+                <div className="meal-info">
+                    <img src={this.props.image} alt={this.props.title} className='meal-image' />
+                    <div className="">
+                        <strong className="meal-title">{this.props.title}</strong>
+                        <p className="meal-desc">{this.props.desc}</p>
+                    </div>
+                    <hr className='solid-line' />
+                </div>
+                <p className='meal-price'>Rs {this.props.price}</p>
+            </div>
+        )
+    }
+}
+
+export default MealCard;
