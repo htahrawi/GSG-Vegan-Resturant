873b02c3c93fa07276b9ead7617d3db4a87a798f
htahrawi <h.tahrawi01@gmail.com>
2023-07-04 15:04:20 +0300

    add the search icon in navbar

--- src/pages/Home-page/sections/WelcomeSection/index.jsx
+++ src/pages/Home-page/sections/WelcomeSection/index.jsx
@@ -15,6 +15,7 @@ class WelcomeSection extends Component {
                                 <li><a href='#special'><span>Special</span></a></li>
                                 <li><a href='#menu'><span>Menu</span></a></li>
                                 <li><a href='#blogs'><span>Blogs</span></a></li>
+                                <li><a href='#blogs'><img src="/assets/search.png" alt="" /></a></li>
                             </ul>
                             <button className='booking-btn'>Booking now</button>
                         </nav>
